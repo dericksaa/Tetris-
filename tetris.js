0b7e1d6775d97d34171b4095af3e6e2aa1c7d70a
Derick Saaa <dmsaao@unal.edu.co>
2022-11-26 18:04:55 -0500

    botones funcionales y posible touch funcional

--- tetris.js
+++ tetris.js
@@ -8,6 +8,14 @@ let ctx = canvas.getContext("2d")
 ctx.scale(ARISTA, ARISTA) 
 let model = new GameModel(ctx)
 
+/*variables para llamar a los botones */
+
+let izquierda = document.querySelector('#izquierda')
+let rotacion = document.querySelector('#rotacion')
+let derecha = document.querySelector('#derecha')
+let abajo = document.querySelector('#abajo')
+
+
 let score = 0 
 
 setInterval(() => {
@@ -74,3 +82,42 @@ document.addEventListener("keydown", (e) => {
             break
     }
 })
+
+//* MODIFICACION DE CODIGO*/
+/*Se crea un evento de escucha para el click en los botones 
+el cual llama a las funciones ubicadas en el archivo gamemodel
+las cuales hacen posible el movimiento de las fichas*/
+rotacion.addEventListener('click', rotar)
+function rotar (){
+    model.rotate()
+}
+izquierda.addEventListener('click', moveLeft)
+function moveLeft (){
+    model.move(false)
+}
+derecha.addEventListener('click', moveRight)
+function moveRight (){
+    model.move(true)
+}
+abajo.addEventListener('click', moveAbajo)
+function moveAbajo (){
+    model.moveDown() 
+}
+
+
+rotacion.addEventListener('touchenter', rotar)
+function rotar (){
+    model.rotate()
+}
+izquierda.addEventListener('touchenter', moveLeft)
+function moveLeft (){
+    model.move(false)
+}
+derecha.addEventListener('touchenter', moveRight)
+function moveRight (){
+    model.move(true)
+}
+abajo.addEventListener('touchenter', moveAbajo)
+function moveAbajo (){
+    model.moveDown() 
+}
